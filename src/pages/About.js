896fabdb173135d1421a1af3eb73956c936165ee
Henry Mickael <henry.mickael1@gmail.com>
2023-04-23 00:02:30 +0200

    page about brut, sans collaps ni css

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,23 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import Banner from "../components/Banner";
+import image2 from "../assets/IMGB.png";
+import "../style/About.css";
+import aboutData from "../aboutData";
 
 const About = () => {
   return (
-    <div>
-      <p>About</p>
-      <Link to="/">Acceuil</Link>
+    <div className="about">
+      <Banner imageUrl={image2} />
+      <div className="about_Collaps">
+        <h2>Fiabilité</h2>
+        <p>{aboutData.fiabilite}</p>
+        <h2>Respect</h2>
+        <p>{aboutData.respect}</p>
+        <h2>Service</h2>
+        <p>{aboutData.service}</p>
+        <h2>Sécurité</h2>
+        <p>{aboutData.securite}</p>
+      </div>
     </div>
   );
 };
